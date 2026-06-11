4ee9bf5ed4177e25f4b09e460a0ebf027d873f7d
vickyvillalba22 <villalbavictoria120@gmail.com>
2026-06-11 19:44:44 -0300

    alguna cosa

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section>
+        <h3>best</h3>
+        <h1>burgers</h1>
+    </section>
+  )
+}
+
+export default HeroSection
